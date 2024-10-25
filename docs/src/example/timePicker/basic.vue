91fbcb0d9efec978f934cb0438cad68c26b31da8
Sam <llh@birdpaper.com>
2024-10-25 14:11:02 +0800

    :memo: docs(docs): vitepress version and timePicker doc

--- docs/src/example/timePicker/basic.vue
+++ docs/src/example/timePicker/basic.vue
@@ -1,12 +1,11 @@
 <template>
-  <!-- {{ time }} -->
   <bp-space type="vertical">
-    <bp-time-picker v-model="time" :style="{ width: '180px' }" placeholder="请选择"></bp-time-picker>
+    <bp-time-picker v-model="time" :style="{ width: '180px' }" clearable placeholder="请选择"></bp-time-picker>
   </bp-space>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-const time = ref("11:21:01");
+const time = ref("");
 </script>
